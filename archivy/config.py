ed3696e73535716e952d18c982d0ba134aa8fcb2
zast <zast@users.noreply.github.com>
2026-09-04 15:13:35 +0000

    zast-cve-benchmark: archivy-v1.6.1 source and spec reports (single commit)

---
+++ archivy/config.py
@@ -0,0 +1,148 @@
+import os
+import appdirs
+
+
+class Config(object):
+    """Configuration object for the application"""
+
+    def __init__(self):
+        self.SECRET_KEY = os.urandom(32)
+        self.PORT = 5000
+        self.HOST = "127.0.0.1"
+        self.INTERNAL_DIR = appdirs.user_data_dir("archivy")
+        self.USER_DIR = self.INTERNAL_DIR
+        self.DEFAULT_BOOKMARKS_DIR = ""
+        self.SITE_TITLE = "Archivy"
+        os.makedirs(self.INTERNAL_DIR, exist_ok=True)
+
+        self.PANDOC_HIGHLIGHT_THEME = "pygments"
+        self.SCRAPING_CONF = {"save_images": False}
+
+        self.THEME_CONF = {
+            "use_theme_dark": False,
+            "use_custom_css": False,
+            "custom_css_file": "",
+        }
+
+        self.EDITOR_CONF = {
+            "settings": {
+                "html": False,
+                "xhtmlOut": False,
+                "breaks": False,
+                "linkify": True,
+                "typographer": False,
+            },
+            "plugins": {
+                "markdownitFootnote": {},
+                "markdownitMark": {},
+                "markdownItAnchor": {"permalink": True, "permalinkSymbol": "¶"},
+                "markdownItTocDoneRight": {},
+            },
+        }
+
+        self.SEARCH_CONF = {
+            "enabled": 0,
+            "url": "http://localhost:9200",
+            "index_name": "dataobj",
+            "engine": "",
+            "es_user": "",
+            "es_password": "",
+            "es_processing_conf": {
+                "settings": {
+                    "highlight": {"max_analyzed_offset": 100000000},
+                    "analysis": {
+                        "analyzer": {
+                            "rebuilt_standard": {
+                                "stopwords": "_english_",
+                                "tokenizer": "standard",
+                                "filter": ["lowercase", "kstem", "trim", "unique"],
+                            }
+                        }
+                    },
+                },
+                "mappings": {
+                    "properties": {
+                        "title": {
+                            "type": "text",
+                            "analyzer": "rebuilt_standard",
+                            "term_vector": "with_positions_offsets",
+                        },
+                        "tags": {"type": "text", "analyzer": "rebuilt_standard"},
+                        "body": {"type": "text", "analyzer": "rebuilt_standard"},
+                    }
+                },
+            },
+        }
+
+    def override(self, user_conf: dict, nested_dict=None):
+        """
+        This function enables an override of the default configuration with user values.
+
+        Acts smartly so as to only set options already set in the default config.
+
+        - user_conf: current (nested) dictionary of user config key/values
+        - nested_dict: reference to the current object that should be modified.
+            If none it's just a reference to the current Config itself, otherwise it's a nested dict of the Config
+        """
+        for k, v in user_conf.items():
+            if (nested_dict and not k in nested_dict) or (
+                not nested_dict and not hasattr(self, k)
+            ):
+                # check the key is indeed defined in our defaults
+                continue
+            curr_default_val = nested_dict[k] if nested_dict else getattr(self, k)
+            if type(v) is dict:
+                # pass on override to sub configuration dictionary if the current type of value being traversed is dict
+                self.override(v, curr_default_val)
+            else:
+                # otherwise just set
+                if type(curr_default_val) == list and type(v) == str:
+                    v = v.split(", ")
+                if nested_dict:
+                    nested_dict[k] = v
+                else:
+                    setattr(self, k, v)
+
+
+class BaseHooks:
+    """
+    Class of methods users can inherit to configure and extend archivy with hooks.
+
+
+    ## Usage:
+
+    Archivy checks for the presence of a `hooks.py` file in the
+    user directory that stores the `data/` directory with your notes and bookmarks.
+    This location is usually set during `archivy init`.
+
+    Example `hooks.py` file:
+
+    ```python
+    from archivy.config import BaseHooks
+
+    class Hooks(BaseHooks):
+        def on_edit(self, dataobj):
+            print(f"Edit made to {dataobj.title}")
+
+        def before_dataobj_create(self, dataobj):
+            from random import randint
+            dataobj.content += f"\\nThis note's random number is {randint(1, 10)}"
+
+        # ...
+    ```
+
+    If you have ideas for any other hooks you'd find useful if they were supported,
+    please open an [issue](https://github.com/archivy/archivy/issues).
+    """
+
+    def on_dataobj_create(self, dataobj):
+        """Hook for dataobj creation."""
+
+    def before_dataobj_create(self, dataobj):
+        """Hook called immediately before dataobj creation."""
+
+    def on_user_create(self, user):
+        """Hook called after a new user is created."""
+
+    def on_edit(self, dataobj):
+        """Hook called whenever a user edits through the web interface or the API."""
---
+++ docs/editing.md
@@ -0,0 +1,61 @@
+## Format
+
+Archivy files are in the [markdown](https://daringfireball.net/projects/markdown/basics) format following the [commonmark spec](https://spec.commonmark.org/).
+
+We've also included a few powerful extensions:
+
+- **Bidirectional links**: You can easily link to a new note in the web editor by typing `[[`: an input box will appear where you can enter the title of the note you want to link to. Otherwise, links between notes are in the format `[[linked note title|linked note id]]`.
+
+- **Embedded tags**: You can directly add tags inside your notes with this `#tag#` syntax (see below). These tags and their groupings can then be viewed by clicking `Tags` on the navigation bar. Starting to type `#` in the web editor will display an input where you can search existing tags.
+
+```md
+I was going to a #python# conference, when I saw a #lion#.
+```
+
+- **In-editor bookmarking**: if you'd like to store a local copy of a webpage you're referring to inside an archivy note, simply select the url and click the "bookmark" icon.
+
+- **LaTeX**: you can render mathematical expressions like this:
+
+```md
+$$
+\pi = 3.14
+$$
+```
+
+- **Footnotes**:
+	```md
+	What does this describe? [^1]
+
+	[^1]: test foot note.
+	```
+
+- **Tables**:
+	```md
+	| Column 1 | Column 2 |
+	| -------- | -------- |
+	| ...      | ...      |
+	```
+
+- **Code blocks with syntax highlighting**:
+	````md
+	```python
+	print("this will be highlighted")
+	x = 1337
+	```
+	````
+
+There are several ways you can edit content in Archivy.
+
+Whenever you open a note or bookmark, at the bottom of the page you'll find a few buttons that allow you to edit it.
+
+## Editing through the web interface
+
+You can edit through the web app, by clicking "Toggle web editor" at the bottom. This is the recommended way because the Archivy web editor provides useful functionality. You can save your work, link to other notes with the "Link to a note button", and archive webpages referenced in your note, all inside the editor!
+
+## Locally
+
+You can do a **local edit**. This option is only viable if running archivy on your own computer. This will open the concerned file with the default app set to edit markdown.
+
+For example like this:
+
+![ex of local editing with marktext](img/local-edit.png)
